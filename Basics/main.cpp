370597777466130820263652131d0a1b92a9ba3c
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-02 09:28:28 -0500

    Basics: statements

---
+++ Basics/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/*
+Statements:
+    - A computer program is a sequence of instructions that tell the computer what to do
+    - A statement is a type of instruction (and the most common type, in C++) that tells the program to perform some action.
+    - Statements are the smallest independent unit of computation in the C++ language
+        - In other words, a statement is the smallest piece of code that can be executed on its own, and treated as an instruction
+        - This also means that the pieces of a statement cannot execute independently
+    Types of statements (more on this later):
+        - Declaration statements
+        - Jump statements
+        - Expression statements
+        - Compound statements
+        - Selection statements (conditionals)
+        - Iteration statements (loops)
+        - Try blocks
+*/
+
+int main() {
+    std::cout << "Hello world!";
+    return 0;
+}
