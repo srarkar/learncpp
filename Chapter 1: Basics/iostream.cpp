2a7f9729a669ffbbbd8e4224005b909fa4120898
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-12 13:59:52 -0500

    std::cin overview

--- Chapter 1: Basics/iostream.cpp
+++ Chapter 1: Basics/iostream.cpp
@@ -19,12 +19,25 @@
             - Since std::endl implicitly calls flush after the newline character, it can be inefficient, since it could force multiple flushes that are not needed
             - Best to use if we want to see the output immediately rather than waiting for the next natural flush
             - Otherwise, simply append a newline character ('\n') to move the cursor to the next line without force-flushing
+
+        std::cin -> cin = character input. reads input from the keyboard
+                    uses the extraction operator (>>) to place input data into a variable
+                        - skips leading whitespace
+                        - if the input buffer is now empty, >> will wait for the user to enter more data.
+                        - >> then extracts as many consecutive characters as it can, until it encounters either a newline character 
+                            or a invalid character for the variable being extracted to (such as a space char when loading into an integer)
+                            If this step is completed, extraction is a success. if this step is reached but no chars extracted, then 
+                            the object being extracted to is copy-assigned the value 0, and any future extractions will immediately fail until std::cin is cleared
+                    Much like std::cout, std::cin is buffered
+                    The extraction operator removes from the front of the input buffer and stores it into a variable via copy-assignment
+
+                    cin being buffered allows us to take a single input (ex: "4 5") and extract multiple times from it (x = 4, y = 5)
 */
 
 
 
 int main() {
-    int x{ 5 };
+    int a{ 5 };
 
     // these will all print on the same line! We need to output a newline character in order to move the cursor to the next line
     std::cout << "Hello world!";
@@ -32,7 +45,7 @@ int main() {
 
     std::cout << 4;
 
-    std::cout << 5;
+    std::cout << a;
 
     std::cout << "Hello world!" << std::endl; // fine, but forces a std::flush
     
@@ -41,8 +54,25 @@ int main() {
     std::cout << "Hello world!\n"; 
 
     /* Author note about using double quotes for character in outputs:
-    - It’s simpler to double-quote all outputted text rather than having to determine what should be single-quoted and double-quoted.
-    - More importantly, it helps avoid inadvertent multicharacter literals (see Datatypes)
+        - It’s simpler to double-quote all outputted text rather than having to determine what should be single-quoted and double-quoted.
+        - More importantly, it helps avoid inadvertent multicharacter literals (see Datatypes)
     */
+
+    int x{};
+
+    std::cout << "Enter a number: "; // newline or endl is not needed, since user pressing "enter" will move to new line
+
+    std::cin >> x;
+
+    std::cout << "You entered: " << x << '\n';
+
+
+    int y{};
+    int z{};
+    std::cout << "Now enter two numbers: ";
+    std::cin >> y >> z; // get two numbers and store in variable x and y respectively
+
+    std::cout << "You entered " << y << " and " << z << '\n';
+
     return 0;
 }
