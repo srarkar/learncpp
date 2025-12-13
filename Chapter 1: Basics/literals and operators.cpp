6934c8ce5e81021dcb8623dab526bc44785c8c2c
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-12 22:33:08 -0500

    expressions

--- Chapter 1: Basics/literals and operators.cpp
+++ Chapter 1: Basics/literals and operators.cpp
@@ -17,6 +17,25 @@ Operators
         Operators can have multiple contexts. For example, operator- can be applied to 1, or 2 operands (sign inversion vs subtraction)
 */
 
+/*
+Expressions
+    An expression is a non-empty sequence of literals, variables, operators, and function calls that calculates a value.
+    The process of executing an expression is an evaluation, and the value calculated is a result or return value.
+    
+    Expressions can also have side effects. 
+        While "x = 5" evaluates to x, it has the side effect of assigning 5 to the variable identified as x
+        In many cases, we use an expression for the side effect rather than the value it produces
+        An expression statement is a statement that consists of an expression followed by a semicolon. 
+            When the expression statement is executed, the expression will be evaluated.
+            The value of the expression itself is discarded. Ex: For x = 5, the operator= value is discarded
+
+    Simplifying a bit, a subexpression is an expression used as an operand. 
+        For example, the subexpressions of x = 4 + 5 are x and 4 + 5. The subexpressions of 4 + 5 are 4 and 5.
+    A full expression is an expression that is not a subexpression. 
+    In casual language, a compound expression is an expression that contains two or more uses of operators. 
+        x = 4 + 5 is a compound expression because it contains two uses of operators (operator= and operator+). 2 and 2 + 3 are not compound expressions.
+*/
+
 
 #include <iostream>;
 
@@ -25,5 +44,10 @@ int main() {
     int x { 5 }; // copy the literal 5 into the memory location associated with variable identified as x
 
     std::cout << 5 << '\n'; // print the value of a literal
+    /*
+    2               // 2 is a literal that evaluates to value 2
+    2 + 3           // 2 + 3 uses operator+ to evaluate to value 5
+    x = 4 + 5       // 4 + 5 evaluates to value 9, which is then assigned to variable x
+    */
     return 0;
 }
