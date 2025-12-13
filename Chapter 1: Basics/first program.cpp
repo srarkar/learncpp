32722afeab8085068216894290f4e9e4f4291b59
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-13 11:09:17 -0500

    first program

---
+++ Chapter 1: Basics/first program.cpp
@@ -0,0 +1,25 @@
+
+
+/*
+    Expected Behavior:
+        // 4 is the input from the user
+        Enter an integer: 4 
+        Double that number is: 8        
+*/
+
+#include <iostream> 
+
+int main() {
+    // declare integer variable to hold user input 
+    int x{ };
+
+    // get input from user and store into integer variable
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+
+    // print double the user input
+    std::cout << "Double of " << x << " is " << x * 2 << '\n';
+
+
+    return 0;
+}
