fa3ec6714b69728b3c0f34f689e51e54ba69abfd
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-13 21:45:56 -0500

    scope and lifetime

--- Chapter 2: Functions and Files/intro to functions.cpp
+++ Chapter 2: Functions and Files/intro to functions.cpp
@@ -26,6 +26,15 @@ Basic Template of a Function:
         return 0;
     }
 */
+
+/*
+A few basic guidelines for writing functions:
+    - Groups of statements that appear more than once in a program should generally be made into a function. For example, if we’re reading input from the user multiple times in the same way, that’s a great candidate for a function. If we output something in the same way in multiple places, that’s also a great candidate for a function.
+    - Code that has a well-defined set of inputs and outputs is a good candidate for a function, (particularly if it is complicated). For example, if we have a list of items that we want to sort, the code to do the sorting would make a great function, even if it’s only done once. The input is the unsorted list, and the output is the sorted list. Another good prospective function would be code that simulates the roll of a 6-sided dice. Your current program might only use that in one place, but if you turn it into a function, it’s ready to be reused if you later extend your program or in a future program.
+    - A function should generally perform one (and only one) task.
+    - When a function becomes too long, too complicated, or hard to understand, it can be split into multiple sub-functions. This is called refactoring.
+*/
+
 #include <iostream>
 
 // without a forward declaration, a function must be defined before it is called
