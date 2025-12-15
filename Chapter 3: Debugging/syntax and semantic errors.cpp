42a401eddf534b54c28e4bbf3112a3fd93ddbc2a
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-15 10:45:12 -0500

    syntax and semantic errors

---
+++ Chapter 3: Debugging/syntax and semantic errors.cpp
@@ -0,0 +1,42 @@
+/*
+Errors
+    Errors generally fall into one of two categories:
+        - Syntax errors: caused by a statement that is not valid according to the grammar of C++.
+            Examples: missing semicolons, unamtched braces/parenthesis
+            Detected by the compiler, and generally easy to fix
+
+            #include <iostream>
+            int main( // missing closing brace
+            {
+                int 1x; // variable name can't start with number
+                std::cout << "Hi there"; << x +++ << '\n'; // extraneous semicolon, operator+++ does not exist
+                return 0 // missing semicolon at end of statement
+            }
+
+        - Semantic/logical errors: An error in meaning. 
+            A statement can be syntactically valid, but either violates other rules of the language or does not do what the programmer intended
+            Some kinds are caught by the compiler (undeclared variable, type mismatch...)
+            Others only cause problems at runtime, such as a divide by zero
+            
+            It can also be actual behavior not aligning with the intended behavior, such as an add() function performing subtraction
+
+
+        int add(int x, int y) // this function is supposed to perform addition
+        {
+            return x - y; // but it doesn't due to the wrong operator being used
+        }
+        int main()
+        {
+            int a { 10 };
+            int b { 0 };
+            std::cout << a << " / " << b << " = " << a / b << '\n'; // division by 0 is undefined in mathematics
+
+            int x;
+            std::cout << x; // undefined behavior from use of an uninitialized variable
+
+            5 = y; // x not declared, cannot assign a value to 5
+            
+            return "hello"; // "hello" cannot be converted to an int
+        }
+
+*/
