a8e961f93c3b5cd248463910ffed449114633a19
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-14 20:41:05 -0500

    designing and implementing a program

---
+++ Chapter 2: Functions and Files/designing a program.cpp
@@ -0,0 +1,30 @@
+/*
+Design
+    1. Define the goal
+        - usually as a user-facing outcome
+    2. Define requirements
+        - constraints a solution must abide by
+    3. Define tools, targets, and backup plans
+        - target architecture/OS
+        - working alone or within a team
+        - testing/feedback/release strategy
+        - how code will be backed up
+    4. Break hard problems into easy problems
+        - top-down problem solving 
+        - alternatively, start with easy tasks and arrange them hierarchically (bottom-up)
+    5. Determine sequence of events
+Implementation
+    1. Outline the main() function
+    2. Implement each function 
+    3. Test program and ensure outputs match expected values
+Words of Advice
+    - Keep programs simple to start
+    - Add features over time, focusing on one area at a time
+    - Test each piece of code as you go
+    - Don't invest in perfect early code
+        - Polish later once it is more fleshed out. Early effort is punished when code changes become necessary
+    - Optimize for maintainability over performance
+        - "premature optimization is the root of all evil"
+        - it is worth removing redundancies, modularizing code structure, and making it easy to understand and add to
+        - from there, debugging optimization will be far easier to do later
+*/
