8084e5cc8972dd0b206f49aeb4a668aa4c009ea6
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-15 20:01:45 -0500

    debugging and static analysis

--- Chapter 3: Debugging/debugging.cpp
+++ Chapter 3: Debugging/debugging.cpp
@@ -20,4 +20,54 @@ Finding the cause
         The bug must lie somewhere between the start and end of the program. 
         We can start at some part of the code, and check if the error exists at runtime at that part.
         If it doesn't then the bug exists somewhere below it, and if it does then it exists above it.
+    
+    Tactics 
+        - Comment out code -> reduces the amount of code needed to search through. if issue persists, bug is likely not in commented code
+        - Validate code flow -> adding prints at the top of functions shows when they are called, and if they are being called too little or too much
+        - Printing values -> we can also print the values of variables and verify they are as expected, using std::cerr and an insertion operator.
+    
+        However, debug prints can clutter code, and must be removed before actually running the program. 
+
+        One strategy is to use preprocessor directives (#define, #ifdef) to conditionally allow debug prints to run.
+        This makes code even more cluttered, but gives control over when debugs should be ran or not.
+
+    Logging
+        A log is a sequential record of events that have happened. They are typically written to a portion of disk called a log file.
+        C++ has an output stream called std::clog intended for writing logging info. By default, it shares a stream with std::cerr
+        Many logging tools have built-in support for enabling/disabling debugging, removing the need for directives for it
+        Some examples are plog and, in some large/performance-sensitive projects, spdlog
 */
+
+/*
+Using a Debugger
+    When running a program, the value of the variables you’re using, which functions have been called, and the current point of execution within the program are known as the program state.
+
+    Currently we have only explored what to do with information after the program has run to completion, not in the middle of execution.
+    A debugger allows us to step through a program and view or even modify program state to control its execution and determine where a bug is occurring.
+*/
+
+/*
+Testing
+    It is harder to debug code than it is to write it.
+    As such, it is better to write readable and maintainable code, and defensively anticipate what might go wrong and code workarounds for them.
+    In addition, some tools such as static analysis tools look for semantic errors without executing (hence why they are static)
+    Unit tests, using things like assert or static assert, can verify some result of a function is what it is expected to be.
+*/
+
+
+#include <iostream>
+
+#define ENABLE_DEBUG // comment this line out to disable debug prints
+
+int main() {
+    #ifdef ENABLE_DEBUG
+    std::cerr << "Beginning of main";
+    #endif
+
+    std::cout << "goodbye world" << '\n';
+
+    #ifdef ENABLE_DEBUG
+    std::cerr << "End of main";
+    #endif
+    return 0;
+}
