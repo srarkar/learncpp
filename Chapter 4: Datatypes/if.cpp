97b94afb917d95ae91a3c226d0a360caccb3e8e6
Rick Sarkar <rickdsarkar@gmail.com>
2025-12-17 22:04:12 -0500

    if statements

---
+++ Chapter 4: Datatypes/if.cpp
@@ -0,0 +1,36 @@
+/*
+If Statements
+    An if statement allows us to execute one (or more) lines of code only if some condition is true.
+
+    The simplest if statement takes the following form:
+        if (condition) true_statement;
+    A condition (also called a conditional expression) is an expression that evaluates to a Boolean value.
+
+    Non-zero values evalute to true
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x {};
+    std::cin >> x;
+
+    if (x == 0)
+        std::cout << "The value is zero\n";
+    else if (x > 0):
+        std::cout << "The value is positive\n";
+    else:
+        std::cout << "The value is negative\n";
+
+    int y { 4 };
+
+    if (y) // non-zero is evaluated as true
+        std::cout << "hi\n";
+    else:
+        std::cout << "bye\n";
+
+    return 0;
+}
