5e330e13938eb8d28724b6d23d79726ba7901f67
Rick Sarkar <rickdsarkar@gmail.com>
2026-01-02 16:18:28 -0500

    const and constexpr lvalue references

--- Chapter 12: References and Pointers/lvalue references.cpp
+++ Chapter 12: References and Pointers/lvalue references.cpp
@@ -30,6 +30,27 @@ Dangling Reference
     This leaves the reference as "dangling", referring to an object that no longer exists.
 */
 
+/*
+lvalue references to const
+    Adding the const keyword when declaring an lvalue reference tells that the object being referenced should be treated as const.
+    A const reference can be used to read, but not modify, the referent. Note that a const reference can be applied to a non-const object as well.
+
+    Const lvalue references can also bind to rvalues, unlike non-const lvalue references. The reference to const becomes bound to the temporary object created and initialized from the rvalue.
+    Lvalue references to const can even bind to values of a different type, so long as those values can be implicitly converted to the reference type
+
+    When a reference is bound to a temporary, changes made to the original object will not be reflected in the reference.
+    Also, when a const reference is directly bound to a temporary object, it extends the lifetime to match that of the reference.
+        This is to prevent a dangling reference from being created immediately after the end of the expression.
+        
+    Lvalue references can only bind to modifiable lvalues.
+    Lvalue references to const can bind to modifiable lvalues, non-modifiable lvalues, and rvalues. 
+        This makes them a much more flexible type of reference.
+
+    Constexpr references typically don’t see much use. They allow the reference to be used in a constant expression, and can only be bound to objects of static duration (global variables or static locals)
+        They cannot bind to non-static locals since the address of the local variable is not known until the function they are defined in is called, where as static variables are created before main() is called.
+*/
+
+
 #include <iostream>
 
 int main()
@@ -47,6 +68,18 @@ int main()
     // double d { 6.0 };
     // int& invalidRef { d };     // invalid: conversion of double to int is narrowing conversion, disallowed by list initialization
     // double& invalidRef2 { x }; // invalid: non-const lvalue reference can't bind to rvalue (result of converting x to double)
+
+    const int& ref { 5 }; // okay: 5 is an rvalue
+
+    std::cout << ref << '\n'; // prints 5
+
+       int x { 5 };          // x is a modifiable lvalue
+    const int& ref { x }; // okay: we can bind a const reference to a modifiable lvalue
+
+    std::cout << ref << '\n'; // okay: we can access the object through our const reference
+    // ref = 7;                  // error: we can not modify an object through a const reference
+
+    x = 6;                // okay: x is a modifiable lvalue, we can still modify it through the original identifier
 
     return 0;
 }
